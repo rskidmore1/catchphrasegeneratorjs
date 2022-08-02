e16dd409c1b02140abb167fdf2e5eddc2249e616
rskidmore1 <ryan.skidmore@yahoo.com>
2022-08-01 20:42:47 -0700

    Adding catchPhraseGenerator.js.

---
+++ catchPhraseGenerator.js
@@ -0,0 +1,16 @@
+catchPhraseGenerator.js
+
+
+const pronouns = ["I", "He", "They", "It", "Ze"];
+const verbs = ["eats", "drives", "falls", "rocks", "pulverizes"];
+const noun = [
+  "your mom",
+  "your childhood",
+  "the children of the world",
+  "dirty bastard",
+  "us together",
+  "vs code",
+];
+
+// -start here, make this a funciton
+console.log(pronouns[Math.floor(Math.random() * pronouns.length)]);
